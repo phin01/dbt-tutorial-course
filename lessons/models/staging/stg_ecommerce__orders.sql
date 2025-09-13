4957a6e40c5929f001ea594ec7060cb20c480621
Paulo Nascimento <phin01@gmail.com>
2025-09-13 14:07:30 -0300

    rename columns in stg orders

--- lessons/models/staging/stg_ecommerce__orders.sql
+++ lessons/models/staging/stg_ecommerce__orders.sql
@@ -5,13 +5,18 @@ WITH source AS (
 )
 
 SELECT
+  -- IDs
   order_id,
   user_id,
-  status,
+
+  -- Timestamps
   created_at,
   returned_at,
   shipped_at,
   delivered_at,
-  num_of_item
+
+  -- Order details
+  status,
+  num_of_item as num_items_ordered
 
 FROM source
