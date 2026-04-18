d8d3b42d94b5ecef84dcb50b39f179b13091dfb9
Paulo Nascimento <phin01@gmail.com>
2026-04-18 12:07:19 -0300

    create example of incremental model

---
+++ lessons/models/staging/stg_ecommerce__events.sql
@@ -0,0 +1,38 @@
+{{ config(
+        materialized='incremental',
+        unique_key='event_id',
+        on_schema_change='fail'
+    ) 
+}}
+
+WITH source AS (
+	SELECT *
+
+	FROM {{ source('thelook_ecommerce', 'events') }}
+    WHERE created_at >= '2025-01-01'
+)
+
+SELECT
+	id AS event_id,
+	user_id,
+	sequence_number,
+	session_id,
+	created_at,
+	ip_address,
+	city,
+	state,
+	postal_code,
+	browser,
+	traffic_source,
+	uri AS web_link,
+	event_type
+
+FROM source
+
+{# Only runs this filter on an incremental run #}
+{% if is_incremental() %}
+
+{# The {{ this }} macro is essentially a {{ ref('') }} macro that allows for a circular reference #}
+WHERE created_at > (SELECT MAX(created_at) FROM {{ this }})
+
+{% endif %}
